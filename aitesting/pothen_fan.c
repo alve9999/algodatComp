1afa952d3ca3dfe852c4ec1f1ca95064d48e5561
kallehed <kallehed@gmail.com>
2025-04-09 12:08:13 +0200

    added 'pothen_fan.c' to aitesting folder. It almost works. Just replace recursion with stack.

---
+++ aitesting/pothen_fan.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <omp.h>
+#include <limits.h>
+#include <time.h>
+#include <stdatomic.h>
+
+typedef struct {
+    size_t        u;
+    size_t        v;
+} xedge_t;
+
+int dfs_la_ts(int** graph, int* graphColSize,int graphSize,int* lookahead,int u,atomic_int* visited,int* pairA,int* pairB){
+    // TODO: do lookahead thing if we use stack-mechanism instead of recursive.
+    if (u == 0) return 0;
+    for (int i = 0; i < graphColSize[u]; i++) {
+        int v = graph[u][i];
+        if(pairB[v] == 0){
+            if (atomic_fetch_add(&visited[v], 1) == 0) {
+                // found unmatched v, that wasn't visited. Let's match with this one.
+                pairA[u] = v;
+                pairB[v] = u;
+                return v;
+            }
+        }
+    }
+
+    for (int i = 0; i < graphColSize[u]; i++) { // TODO: fariness
+        int v = graph[u][i];
+        if(atomic_fetch_add(&visited[v],1) == 0){
+            int index = dfs_la_ts(graph,graphColSize,graphSize,lookahead,pairB[v],visited,pairA,pairB);
+            if(index != 0){
+                // have u, connected to v, which is matched already!
+                // but we found a better alternative.
+                pairA[u] = v;
+                pairB[v] = u;
+                return index;
+            }
+        }
+    }
+    return 0;
+}
+
+
+int parallel_pothen_fan(int** graph, int graphSize, int* graphColSize, int* pairA, int* pairB){
+    int* lookahead = (int*)malloc(graphSize * sizeof(int));
+    for (int i = 0; i < graphSize; i++) {
+        lookahead[i] = 0;
+    }
+    atomic_int path_found = 1;
+    atomic_int matchings = 0;
+    atomic_int* visited = calloc(graphSize, sizeof(atomic_int));
+    while(path_found){
+        for(int i = 2; i < graphSize; i+=2) {
+            atomic_store(&visited[i], 0);
+        }
+        path_found = 0;
+        #pragma omp parallel for
+        for(int i = 1; i < graphSize; i+=2){ // UNMATCHED u VERTICES
+            if (pairA[i] != 0 || visited[i]) continue;
+            int v = dfs_la_ts(graph,graphColSize,graphSize,lookahead,i,visited,pairA,pairB);
+            if(v != 0){
+                atomic_store(&path_found, 1);
+                atomic_fetch_add(&matchings,1);
+            }
+        }
+    }
+    return matchings;
+}
+
+
+void matchAndUpdate(int** graph, int* graphColSize, int* deg, int* pairA, int* pairB, bool* visited, int u, int* matchingSize) {
+    if (visited[u]) return;
+    visited[u] = true;
+    for (int j = 0; j < graphColSize[u]; j++) {
+        int v = graph[u][j];
+        if (!visited[v]) {
+            visited[v] = true;
+            *matchingSize += 1;
+            pairA[u] = v;
+            pairB[v] = u;
+            for (int i = 0; i < graphColSize[v]; i++) {
+                int w = graph[v][i];
+                deg[w]--;
+                if (deg[w] == 1) {
+                    matchAndUpdate(graph, graphColSize, deg, pairA, pairB, visited, w, matchingSize);
+                }
+            }
+            break;
+        }
+    }
+}
+
+
+int karpSipser(int** graph, int graphSize, int* graphColSize, int* pairA, int* pairB){
+    int* deg = (int*)malloc(graphSize * sizeof(int));
+    memcpy(deg, graphColSize, graphSize * sizeof(int));
+    bool* visited = (bool*)calloc(graphSize, sizeof(bool));
+    int* deg1 = (int*)malloc(graphSize * sizeof(int));
+    int* deg2 = (int*)malloc(graphSize * sizeof(int));
+    int deg1Size = 0;
+    int deg2Size = 0;
+    for (int i = 1; i < graphSize; i+=2) {
+        if (deg[i] == 1) {
+            deg1[deg1Size++] = i;
+        } else{
+            deg2[deg2Size++] = i;
+        }
+    }
+    int matchingSize = 0;
+    for(int i = 0; i < deg1Size; i++) {
+        matchAndUpdate(graph, graphColSize, deg, pairA, pairB, visited, deg1[i], &matchingSize);
+    }
+    for(int i = 0; i < deg2Size; i++) {
+        matchAndUpdate(graph, graphColSize, deg, pairA, pairB, visited, deg2[i], &matchingSize);
+    }
+    return matchingSize;
+}
+
+int maximumBipartiteMatching(int** graph, int graphSize, int* graphColSize) {
+    graphSize++;
+    int* pairA = (int*)malloc((graphSize) * sizeof(int));
+    int* pairB = (int*)malloc((graphSize) * sizeof(int));
+    memset(pairA, 0, (graphSize) * sizeof(int));
+    memset(pairB, 0, (graphSize) * sizeof(int));
+
+
+    clock_t start_time = clock();
+
+    // int init_matchings = karpSipser(graph, graphSize, graphColSize, pairA, pairB);
+    int init_matchings = 0;
+    // Stop the timer after karpSipser and before hopcroftKarp
+    clock_t end_time_1 = clock();
+    double time_karpSipser = (double)(end_time_1 - start_time) / CLOCKS_PER_SEC;
+    //printf("Time taken by karpSipser: %f seconds\n", time_karpSipser);
+
+    // Now, call hopcroftKarp
+    start_time = clock();
+    int main_matchings = parallel_pothen_fan(graph, graphSize, graphColSize,pairA, pairB);
+    //int main_matchings = hopcroftKarp(graph, graphSize, graphColSize, pairA, pairB);
+
+    // Stop the timer after hopcroftKarp
+    clock_t end_time_2 = clock();
+    double time_hopcroftKarp = (double)(end_time_2 - start_time) / CLOCKS_PER_SEC;
+    //printf("Time taken by hopcroftKarp: %f seconds\n", time_hopcroftKarp);
+    //printf("%d %d\n", init_matchings, main_matchings);
+    return init_matchings + main_matchings;
+}
+
+int** createGraphFromArrays(size_t n, size_t m, xedge_t e[]) {
+    int* edgeCounts = (int*)calloc(n, sizeof(int));
+
+
+    for (size_t i = 0; i < m; i++) {
+        if (e[i].u < n) edgeCounts[e[i].u]++;
+        if (e[i].v < n) edgeCounts[e[i].v]++;
+    }
+
+
+    int** graph = (int**)malloc(n * sizeof(int*));
+    for (size_t i = 0; i < n; i++) {
+        graph[i] = (int*)malloc(edgeCounts[i] * sizeof(int));
+    }
+
+    int* currentIndex = (int*)calloc(n, sizeof(int));
+
+
+    for (size_t i = 0; i < m; i++) {
+        if (e[i].u < n)
+            graph[e[i].u][currentIndex[e[i].u]++] = e[i].v;
+        if (e[i].v < n)
+            graph[e[i].v][currentIndex[e[i].v]++] = e[i].u;
+    }
+
+
+    free(edgeCounts);
+    free(currentIndex);
+
+    return graph;
+}
+
+size_t matching(size_t n, size_t m, xedge_t e[]) {
+    int* edgeCounts = (int*)calloc(n, sizeof(int));
+    for (size_t i = 0; i < m; i++) {
+        edgeCounts[e[i].u]++;
+        edgeCounts[e[i].v]++;
+    }
+    int** graph = (int**)malloc((n+1) * sizeof(int*));
+
+    for (size_t i = 0; i < (n+1); i++) {
+        graph[i] = (int*)malloc(edgeCounts[i] * sizeof(int));
+
+    }
+    int* currentIndices = (int*)calloc((n+1), sizeof(int));
+
+    for (size_t i = 0; i < m; i++) {
+        if (e[i].u < n) {
+            graph[e[i].u][currentIndices[e[i].u]++] = e[i].v;
+        }
+        if (e[i].v < n) {
+            graph[e[i].v][currentIndices[e[i].v]++] = e[i].u;
+        }
+    }
+    free(currentIndices);
+
+    size_t maxMatching = maximumBipartiteMatching(graph, n, edgeCounts);
+
+    return maxMatching;
+}
